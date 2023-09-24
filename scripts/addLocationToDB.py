2f4d1d398ceb01116d899f1f1019be59844593df
Saurabh Pandey <40917276+Pandey-SaurabhP@users.noreply.github.com>
2023-09-24 20:20:34 +0530

    Added Device price evaluation page

--- scripts/addLocationToDB.py
+++ scripts/addLocationToDB.py
@@ -1,20 +1,21 @@
+# Can cause unnecessary data insertion in database
 
 import pymongo
 
 mongodb_url = 'mongodb+srv://pandeygrocks:Saurabh04@maindb.ijbfr2l.mongodb.net/?retryWrites=true&w=majority'  # Replace with your MongoDB server URL
 database_name = "MainDB"  # Replace with your database name
-collection_name = "testCentre"  # Replace with your collection name
+collection_name = "collectionCentre"  # Replace with your collection name
 
-def pushToDB(data):
+def pushToDB(data):     
     try:
         client = pymongo.MongoClient(mongodb_url)
         database = client[database_name]
         collection = database[collection_name]
 
-        inserted_data = collection.insert_many(data)
+        inserted_data = collection.insert_one(data)
 
-        if inserted_data.inserted_ids:
-            print("Data inserted successfully. Inserted IDs:", inserted_data.inserted_ids)
+        if inserted_data.inserted_id:
+            print("Data inserted successfully. Inserted ID:", inserted_data.inserted_id)
         else:
             print("Failed to insert data.")
 
@@ -26,14 +27,14 @@ def pushToDB(data):
         client.close()
 
 def readFromFile():
-    with open('LocationData/west.txt', 'r') as file:
+    with open('LocationData/east.txt', 'r') as file:
         lines = file.readlines()
         lines = [s.replace('\n', '') for s in lines]
 
         id = 1
 
         dict = {}
-        a = ['centre', 'lat', 'long', 'gmapLink']
+        a = ['lat', 'long', 'centre', 'gmapLink']
         i = 0
 
         ans = []
@@ -41,7 +42,7 @@ def readFromFile():
         for j in lines:
             x = j
 
-            if i == 1:
+            if i == 0:
                 lat, long = map(float, j.split(','))
                 # print(lat, long)
                 dict[a[i]] = lat
@@ -54,14 +55,40 @@ def readFromFile():
             
             if(i >= 4):
                 i = 0
-                dict['_id'] = str(id)
                 id += 1
-                ans.append(dict)
-        # print(ans)
-
-        pushToDB(ans)
+                print(dict)
+                # pushToDB(dict)
+                break
+        print(ans)
 
         
+
+def checkKey():
+    try:
+        # Connect to MongoDB
+        client = pymongo.MongoClient(mongodb_url)
+        database = client[database_name]
+        collection = database[collection_name]
+
+        # Get a sample document from the collection
+        sample_document = collection.find_one()
+
+        if sample_document:
+            # Extract the keys (field names) from the sample document
+            key_attributes = list(sample_document.keys())
+            print("Key attributes in the document:", key_attributes)
+        else:
+            print("Collection is empty or doesn't exist.")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+    finally:
+    # Close the MongoDB connection
+        client.close()
+
+# checkKey()
+# pushToDB()
 
 readFromFile()
 
